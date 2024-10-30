a8673a05263e9b909478c5ab04fb52c5456f1a64
Fabián Machuca <machucagelvez@gmail.com>
2024-10-30 11:52:42 -0500

    navbar ajusted

--- proyecto_web/urls.py
+++ proyecto_web/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("apps.main_app.urls")),
+    path("", include("apps.main_app.urls")),
 ]
