753f3d519944de7f6a5b7c4fd556a6b369aee296
Fabián Machuca <machucagelvez@gmail.com>
2024-11-18 09:19:11 -0500

    blog categories

--- apps/blog_app/urls.py
+++ apps/blog_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import blog
+from .views import blog, categories
 
 urlpatterns = [
     path("", blog, name="blog"),
+    path("categories/<int:category_id>", categories, name="categories"),
 ]
--- apps/blog_app/views.py
+++ apps/blog_app/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from apps.blog_app.models import Post
+from apps.blog_app.models import Post, Category
 
 
 def blog(request):
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {"posts": posts})
+
+
+def categories(request, category_id):
+    category = Category.objects.get(id=category_id)
+    posts = Post.objects.filter(categories=category)
+    return render(
+        request, "blog/categories.html", {"category": category, "posts": posts}
+    )
