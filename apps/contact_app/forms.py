627af68ae336c78a510de0a7fabd075f500fc2e5
Fabián Machuca <machucagelvez@gmail.com>
2025-01-08 10:32:42 -0500

    contact form

--- apps/contact_app/forms.py
+++ apps/contact_app/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label="Nombre")
     email = forms.EmailField(required=True, label="Email")
-    content = forms.CharField(label="Contenido")
+    content = forms.CharField(label="Contenido", widget=forms.Textarea)
